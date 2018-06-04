51bb128b666623823caf2d627c941cf400e54854
sahan <sahan@LAPTOP-PN70LU10>
2018-06-04 12:07:36 +0530

    commit 2

---
+++ sampal1/src/main/java/ui1/sampal1/App.java
@@ -0,0 +1,13 @@
+package ui1.sampal1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
